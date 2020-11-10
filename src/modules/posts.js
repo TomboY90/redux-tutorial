219b0788837f865b85d48785784ddfbedb85626d
gmlee <gmlee@salin.co.kr>
2020-11-10 18:26:43 +0900

    1st refactoring :: create thunk, create state

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,4 +1,5 @@
 import * as postsAPI from '../api/posts';
+import { createPromiseThunk, reducerUtils } from '../lib/asyncUtils';
 
 // Action Type
 const GET_POSTS = 'GET_POSTS';
@@ -9,96 +10,80 @@ const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_FAIL = 'GET_POST_FAIL'
 
-export const getPosts = () => async dispatch => {
-  dispatch({ type: GET_POSTS }); // 요청이 시작됨
+// export const getPosts = () => async dispatch => {
+//   dispatch({ type: GET_POSTS }); // 요청이 시작됨
 
-  try {
-    const posts = await postsAPI.getPosts();
-    dispatch({ type: GET_POSTS_SUCCESS, posts });
-  } catch (e) {
-    dispatch({ type: GET_POSTS_ERROR, error: e });
-  }
-};
+//   try {
+//     const posts = await postsAPI.getPosts();
+//     dispatch({ type: GET_POSTS_SUCCESS, posts });
+//   } catch (e) {
+//     dispatch({ type: GET_POSTS_ERROR, error: e });
+//   }
+// };
 
-export const getPost = id => async(dispatch) => {
-  dispatch({ type: GET_POST });
+// export const getPost = id => async(dispatch) => {
+//   dispatch({ type: GET_POST });
 
-  try {
-    const post = await postsAPI.getPostById(id);
-    dispatch({ type: GET_POST_SUCCESS, post });
-  } catch (e) {
-    dispatch({ type: GET_POST_FAIL, error: e })
-  }
-}
+//   try {
+//     const post = await postsAPI.getPostById(id);
+//     dispatch({ type: GET_POST_SUCCESS, post });
+//   } catch (e) {
+//     dispatch({ type: GET_POST_FAIL, error: e })
+//   }
+// }
+
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
 
 const initialState = {
-  posts: {
-    loading: false,
-    data: null,
-    error: null
-  },
-  post: {
-    loading: false,
-    data: null,
-    error: null
-  }
-}
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial()
+};
+
+// const initialState = {
+//   posts: {
+//     loading: false,
+//     data: null,
+//     error: null
+//   },
+//   post: {
+//     loading: false,
+//     data: null,
+//     error: null
+//   }
+// }
 
 export default function posts(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
       return {
         ...state,
-        posts: {
-          loading: true,
-          data: null,
-          error: null
-        }
+        posts: reducerUtils.loading()
       };
     case GET_POSTS_SUCCESS:
       return {
         ...state,
-        posts: {
-          loading: true,
-          data: action.posts,
-          error: null
-        }
+        posts: reducerUtils.success(action.payload)
       };
     case GET_POSTS_ERROR:
       return {
         ...state,
-        posts: {
-          loading: true,
-          data: null,
-          error: action.error
-        }
+        posts: reducerUtils.error(action.error)
       };
     case GET_POST:
       return {
         ...state,
-        post: {
-          loading: true,
-          data: null,
-          error: null
-        }
+        post: reducerUtils.loading()
       };
     case GET_POST_SUCCESS:
       return {
         ...state,
-        post: {
-          loading: true,
-          data: action.post,
-          error: null
-        }
+        post: reducerUtils.success(action.payload)
       };
     case GET_POST_ERROR:
       return {
         ...state,
-        post: {
-          loading: true,
-          data: null,
-          error: action.error
-        }
+        post: reducerUtils.error(action.error)
       };
   }
 }
